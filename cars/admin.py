c251f553239b1d4ccd4cc0f9856f9aa60fc34181
bibek977 <bhattaraibibek91@gmail.com>
2024-07-15 11:14:59 +0545

    cars models created

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from cars.models import *
 
-# Register your models here.
+class CarsModelAdmin(admin.ModelAdmin):
+    list_display = ['id','brand','color','liscense_no']
+
+admin.site.register(Cars,CarsModelAdmin)
+
+class SpaceModelAdmin(admin.ModelAdmin):
+    list_display = ['id','space_box']
+
+admin.site.register(Space,SpaceModelAdmin)
+
+class ParkSlotModelAdmin(admin.ModelAdmin):
+    list_display = ['id','car','space','date']
+
+admin.site.register(ParkSlot,ParkSlotModelAdmin)
--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 from django.utils.translation import gettext_lazy as _
+from datetime import date
+from django.utils import timezone
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
@@ -14,3 +16,26 @@ class Cars(BaseModel):
     brand = models.CharField(_("brand_name"),max_length=200)
     color = models.CharField(_("color"),max_length=200)
     liscense_no = models.CharField(_("liscense_no"),max_length=50)
+
+    def __str__(self):
+        return self.liscense_no
+    
+    class Meta:
+        verbose_name_plural = "Car"
+        verbose_name_plural = "Cars"
+
+
+class Space(BaseModel):
+    space_box = models.CharField(_("space_identity"),max_length=10)
+
+    def __str__(self):
+        return self.space_box
+
+
+class ParkSlot(BaseModel):
+    car = models.ForeignKey(Cars,on_delete=models.CASCADE)
+    space = models.ForeignKey(Space,on_delete=models.CASCADE)
+    date = models.DateField(default=date.today())
+
+    def __str__(self):
+        return str(self.date)
